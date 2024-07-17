4be8fc71ac32387c44de1891924cedc98e06fd41
Fabo2303 <fabian666777@gmail.com>
2024-07-16 22:07:42 -0500

    add user preferences

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let usuario = Object();
+
 function login(){
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -8,8 +10,7 @@ function login(){
     }
 
     console.log(data);
-
-    fetch('https://bajibot-backend.onrender.com/user/login', {
+    fetch('http://127.0.0.1:5000/user/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -22,6 +23,9 @@ function login(){
         if(data.status === 200){
             alert(data.message);
             localStorage.setItem('token', data.token);
+            usuario = data.user;
+            console.log(data);
+            localStorage.setItem('user', JSON.stringify(usuario));
             window.location.href = "chatbot.html";
         } else {
             alert(data.message);
@@ -32,40 +36,53 @@ function login(){
 }
 
 function register() {
-    var username = document.getElementById("register-username").value;
-    var password = document.getElementById("register-password").value;
-    var confirmPassword = document.getElementById("register-password-confirm").value;
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+    const passwordConfirm = document.getElementById('register-password-confirm').value;
+    const favoriteCuisine = document.getElementById('favorite-cuisine').value;
+    const diet = document.getElementById('diet').value;
+    const allergies = document.getElementById('allergies').value;
+    const preferredMealTime = document.getElementById('preferred-meal-time').value;
 
-    if (password !== confirmPassword) {
-        console.error('Error: Passwords do not match.');
+    if (password !== passwordConfirm) {
+        alert('Las contraseñas no coinciden.');
         return;
     }
 
-    var data = {
-        username: username,
-        password: password
+    const preferences = {
+        favorite_cuisine: favoriteCuisine,
+        diet: diet,
+        allergies: allergies.split(',').map(allergy => allergy.trim()),
+        preferred_meal_time: preferredMealTime
     };
 
-    console.log(data);
+    const user = {
+        username: username,
+        password: password,
+        preferences: preferences
+    };
 
-    fetch('https://bajibot-backend.onrender.com/user/insert', {
+    // Lógica para enviar los datos al servidor
+    fetch('http://127.0.0.1:5000/user/insert', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(user)
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.status === 200) {
-            showLogin();
-        } else {
-            console.error('Error:', data.message);
-        }
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Usuario registrado exitosamente');
+                // Redirigir o mostrar mensaje de éxito
+            } else {
+                alert('Error en el registro: ' + data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Error en el registro');
+        });
 }
 
 function showLogin() {
@@ -87,7 +104,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function sendMessage() {
-    var userInput = document.getElementById('user-input').value;
+    const userInput = document.getElementById('user-input').value;
     if (!userInput.trim()) {
         return;
     }
@@ -95,11 +112,11 @@ function sendMessage() {
     appendMessage('me: ' + userInput, 'user');
     document.getElementById('user-input').value = '';
 
-    var data = {
+    const data = {
         message: userInput
     };
 
-    let endpoint = 'https://bajibot-backend.onrender.com/chat';
+    let endpoint = 'http://127.0.0.1:5000/chat';
 
     fetch(endpoint, {
         method: 'POST',
@@ -125,8 +142,8 @@ function sendMessage() {
 }
 
 function appendMessage(message, sender) {
-    var chatBody = document.getElementById('chat-body');
-    var messageDiv = document.createElement('div');
+    const chatBody = document.getElementById('chat-body');
+    const messageDiv = document.createElement('div');
     messageDiv.className = 'message ' + sender;
     messageDiv.textContent = message;
     chatBody.appendChild(messageDiv);
@@ -174,4 +191,74 @@ function formatRecipeResponse(response) {
 
 function returnToLogin() {
     window.location.href = "index.html";
+}
+
+function displayRecommendations() {
+    let endpoint = 'http://127.0.0.1:5000/recommendations';
+
+    usuario = JSON.parse(localStorage.getItem('user'));
+
+    let message = `Dame una lista de 4 recomendaciones de recetas, tengo estas alergias: `;
+    for (let allergy of usuario.preferences.allergies) {
+        message += `${allergy}`;
+    }
+    message += ` tengo un gusto por la comida ${usuario.preferences.favorite_cuisine}`;
+    message += ` y tengo una dieta ${usuario.preferences.diet}`;
+
+    let data = {
+        message: message
+    }
+
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.response) {
+            let recipes = data.response;
+            const recipeList = document.getElementById('recipe-list');
+            console.log(recipes);
+            recipes.forEach(recipe => {
+                const recipeItem = document.createElement('div');
+                recipeItem.className = 'list-group-item';
+                recipeItem.innerHTML = `<h5>${recipe.recipe_name}</h5><p>${recipe.description}</p>`;
+                recipeList.appendChild(recipeItem);
+            });
+        } else {
+            console.log('Error:', data.error);
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+document.getElementById('show-recommendations-btn').addEventListener('click', function() {
+    toggleView('recommendations');
+    displayRecommendations();
+});
+
+
+function toggleView(view) {
+    const recommendations = document.getElementById('recommendations');
+    const chatBox = document.getElementById('chat-box');
+    const showRecommendationsBtn = document.getElementById('show-recommendations-btn');
+    const showChatBtn = document.getElementById('show-chat-btn');
+
+    if (view === 'recommendations') {
+        recommendations.style.display = 'block';
+        chatBox.style.display = 'none';
+        showRecommendationsBtn.style.display = 'none';
+        showChatBtn.style.display = 'block';
+    } else if (view === 'chat') {
+        recommendations.style.display = 'none';
+        chatBox.style.display = 'block';
+        showRecommendationsBtn.style.display = 'block';
+        showChatBtn.style.display = 'none';
+    }
 }
